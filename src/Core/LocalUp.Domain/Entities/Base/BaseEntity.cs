0bb5ba70524df5aed0c26117c155b14cbda01a8a
ArthurTheCreator <arthurbao10@gmail.com>
2025-09-13 13:27:01 -0300

    update: brand

---
+++ src/Core/LocalUp.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace LocalUp.Domain.Entities.Base;
+
+public class BaseEntity<TEntity> where TEntity : BaseEntity<TEntity>
+{
+    public long Id { get; private set; }
+    public DateTimeOffset CreatedDate { get; protected set; }
+    public DateTimeOffset? ChangedDate { get; protected set; }
+    public bool IsActive { get; protected set; }
+
+    protected BaseEntity()
+    {
+        CreatedDate = DateTimeOffset.UtcNow;
+        IsActive = true;
+    }
+
+    public virtual void SetChangedDate()
+    {
+        ChangedDate = DateTimeOffset.UtcNow;
+    }
+}
--- src/Core/LocalUp.Domain/Entities/Brand/Brand.cs
+++ src/Core/LocalUp.Domain/Entities/Brand/Brand.cs
@@ -1,5 +1,36 @@
-﻿namespace LocalUp.Domain.Entities;
+﻿using LocalUp.Domain.Entities.Base;
 
-public class Brand
+namespace LocalUp.Domain.Entities;
+
+public class Brand : BaseEntity<Brand>
 {
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+
+    public List<Product> ListProduct { get; private set; }
+
+    private Brand(string name, string description) : base()
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public Brand Create(string name, string description)
+    {
+        if (name == null) throw new ArgumentNullException("name");
+        if (description == null) throw new ArgumentNullException("description");
+
+        return new Brand(name, description);
+    }
+
+    public void Update(string name, string description)
+    {
+        if (name == null) throw new ArgumentNullException("name");
+        if (description == null) throw new ArgumentNullException("description");
+
+        Name = name;
+        Description = description;
+
+        SetChangedDate();
+    }
 }
